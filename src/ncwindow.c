326cc82c5bd4097a2187a70e965a584d0e1bc791
mes32 <stockman.mike@gmail.com>
2015-07-25 14:05:07 -0400

    Added comments to ncwindow module

--- src/ncwindow.c
+++ src/ncwindow.c
@@ -1,12 +1,26 @@
+/**
+ *  ncwindow.c - folly
+ *
+ *  This module defines the main window used for the game's text-based display. This module primarily serves to 
+ *  configure folly's usage of the 'ncurses' library.
+ *
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <ncurses.h>
 
-#include "ncwindow.h"
 
+// The window width in characters
 static const int WIDTH = 100;
+
+// The window height in characters
 static const int HEIGHT = 100;
 
+
+/**
+ *  Initializes ncurses-based display mode and returns a pointer to the current WINDOW
+ */
 WINDOW* startNCWindow() {
 
 	initscr();
@@ -22,6 +36,9 @@ WINDOW* startNCWindow() {
     return window;
 }
 
+/**
+ *  Closes ncurses-based display mode
+ */
 void endNCWindow() {
 	clrtoeol();
 	refresh();
--- src/ncwindow.h
+++ src/ncwindow.h
@@ -1,10 +1,23 @@
+/**
+ *  ncwindow.h - folly
+ *
+ *  This module defines the main window used for the game's text-based display. This module primarily serves to 
+ *  configure folly's usage of the 'ncurses' library.
+ *
+ */
+
 #ifndef NCWINDOW_H_
 #define NCWINDOW_H_
 
-static const int WIDTH;
-static const int HEIGHT;
 
+/**
+ *  Initializes ncurses-based display mode and returns a pointer to the current WINDOW
+ */
 WINDOW* startNCWindow();
+
+/**
+ *  Closes ncurses-based display mode
+ */
 void endNCWindow();
 
 #endif // NCWINDOW_H_
